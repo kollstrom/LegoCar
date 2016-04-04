c9d71f07d24e324c8f1bb471e84438377dc19d1f
philippz <philippz@stud.ntnu.no>
2016-04-04 16:43:17 +0200

    EV3 code ready, only needs right angles

--- src/demoCar/DemoCar.java
+++ src/demoCar/DemoCar.java
@@ -6,9 +6,12 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import lejos.robotics.RegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.motor.Motor;
 import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
@@ -17,47 +20,321 @@ public class DemoCar{
 		
 		
 		try{
-			int seatAngle = -90;
+			int currentState = 0;
 			boolean ongoing = true;
 			String in, out;
+			
+			//lists for easier change state methods
+			ArrayList<Integer> listOne = new ArrayList<Integer>();
+			ArrayList<Integer> listTwo = new ArrayList<Integer>();
+			ArrayList<Integer> listThree = new ArrayList<Integer>();
+			
+			listOne.add(0); //steeringWheelTilt
+			listOne.add(0); //notInUse, find better solution
+			listOne.add(0); //steeringWheelDepth
+			listOne.add(0); //notInUse, find better solution
+			listOne.add(0); //wingMirrorLeftX
+			
+			listTwo.add(0); //wingMirrorLeftY
+			listTwo.add(0); //notInUse, find better solution
+			listTwo.add(0); //wingMirrorRightX
+			listTwo.add(0); //notInUse, find better solution
+			listTwo.add(0); //wingMirrorRightY
+			
+			listThree.add(0); //seatHeight
+			listThree.add(0); //notInUse, find better solution
+			listThree.add(0); //seatDepth
+			listThree.add(0); //notInUse, find better solution
+			listThree.add(0); //seatBackAngle
+			
+			//the following int values are angles
+			 
+			String radioStation = "NRK P3";
+			
 			System.out.println("Starting program...");
 			ServerSocket serverSocket = new ServerSocket(4321);
 			System.out.println("W8 4 ctrlr 2 start...");
 			Socket servSock = serverSocket.accept();
 			System.out.println("Ctrlr started...");
 			
-			RegulatedMotor seat = new EV3MediumRegulatedMotor(MotorPort.A);
-			seat.setSpeed(90);
-			System.out.println(seat.getSpeed());
+			RegulatedMotor stateChanger = new EV3MediumRegulatedMotor(MotorPort.A);
+			RegulatedMotor mediumMotor = new EV3MediumRegulatedMotor(MotorPort.B);
+			RegulatedMotor largeMotorOne = new EV3LargeRegulatedMotor(MotorPort.C);
+			RegulatedMotor largeMotorTwo = new EV3LargeRegulatedMotor(MotorPort.D);
+			
+			stateChanger.setSpeed(100);
+			mediumMotor.setSpeed(100);
+			largeMotorOne.setSpeed(100);
+			largeMotorTwo.setSpeed(100);
+			
 			BufferedReader input = new BufferedReader(new InputStreamReader(servSock.getInputStream()));
 			PrintStream ps = new PrintStream(servSock.getOutputStream());
+			
+			//How the motors/code should do their/its work
+			// 1. check state, if state right, start motor
+			// 2. if state wrong, change with +-1 with stateChanger
+			// 3. turn motor to right angle for next carThing
+			// 4. change state with stateChanger with +-1
+			// 5. if state right, start motor, else go to 2.
+			
+			//TODO, method for statechanger rotating
+			//TODO, methods for dublicated
+			
 			
 			while (ongoing){
 				System.out.println("W8ing 4 input...");
 				in = input.readLine();
-//				System.out.println("What i got: " + in);
 				
-				if (in.equals("seat")){
-					System.out.println("Waiting for seat angle...");
-					ps.print("Current seat angle " + seatAngle + ". Waiting for seat angle...\n");
-					String angle = input.readLine();
-					if (angle.equals(null) || angle.equals("")){
-						angle = input.readLine();
-					}
-					seatAngle += Integer.parseInt(angle);
-					seat.rotateTo(Integer.parseInt(angle) + 135);
-					ps.print("Angle set to: " + angle + "\n");
-					Delay.msDelay(2000);
+				if (in.equals("seatBackAngle")){
+					System.out.println("Waiting for seatBackAngle...");
 					
+					listThree.set(2, Integer.parseInt(input.readLine()));
+					while (true){
+						
+						if (currentState == 5){
+							//TODO motorX.rotateTo(listThree.get(4);
+							break;
+						}
+						
+						else if ((currentState +1) % 2 == 1){
+							//TODO motorX.rotateTo(listThree.get(currentState + 1));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						else{
+							//currentState is smaller than 5
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 	 
+						}	
+					}
+					Delay.msDelay(2000);
 				}
+				
+				else if (in.equals("seatDepth")){
+					System.out.println("Waiting for seatDepth...");
+					
+					listThree.set(1, Integer.parseInt(input.readLine()));
+					
+					while (true){
+						
+						if (currentState == 3){
+							//TODO motorX.rotateTo(listThree.get(2);
+							break;
+						}
+						
+						else if (currentState == 1){
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;
+							//TODO motorX.rotateTo(listThree.get(2));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						
+						else if (currentState == 5){
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;
+							//TODO motorX.rotateTo(listThree.get(2));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;	
+						}
+					}
+					Delay.msDelay(2000);
+				}
+				
+				else if (in.equals("seatHeight")){
+					System.out.println("Waiting for seatHeight...");
+					
+					listThree.set(0, Integer.parseInt(input.readLine()));
+					
+					while (true){
+						
+						if (currentState == 1){
+							//TODO motorX.rotateTo(listThree.get(0);
+							break;
+						}
+						
+						else if ((currentState -1) % 2 == 1){
+							//TODO motorX.rotateTo(listThree.get(currentState - 1));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						else{
+							//currentState is greater than 1
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 	 
+						}	
+					}
+					
+					Delay.msDelay(2000);
+				}
+				
+				else if (in.equals("wingMirrorRightY")){
+					System.out.println("Waiting for wingMirrorRight Y...");
+					
+					listTwo.set(2, Integer.parseInt(input.readLine()));
+					
+					while (true){
+						
+						if (currentState == 5){
+							//TODO motorX.rotateTo(listTwo.get(4);
+							break;
+						}
+						
+						else if ((currentState +1) % 2 == 1){
+							//TODO motorX.rotateTo(listTwo.get(currentState + 1));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						else{
+							//currentState is smaller than 5
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 	 
+						}	
+					}
+					Delay.msDelay(2000);
+				}
+				
+				else if (in.equals("wingMirrorRightX")){
+					System.out.println("Waiting for wingMirrorRightX...");
+					
+					listTwo.set(1, Integer.parseInt(input.readLine()));
+					
+					while (true){
+						
+						if (currentState == 3){
+							//TODO motorX.rotateTo(listThree.get(2);
+							break;
+						}
+						
+						else if (currentState == 1){
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;
+							//TODO motorX.rotateTo(listThree.get(2));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						
+						else if (currentState == 5){
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;
+							//TODO motorX.rotateTo(listThree.get(2));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;	
+						}
+					}
+					
+					Delay.msDelay(2000);
+				}
+				
+				else if (in.equals("wingMirrorLeftY")){
+					System.out.println("Waiting for wingMirrorLeftY...");
+					
+					listTwo.set(0, Integer.parseInt(input.readLine()));
+					
+					while (true){
+						
+						if (currentState == 1){
+							//TODO motorX.rotateTo(listTwo.get(0);
+							break;
+						}
+						
+						else if ((currentState -1) % 2 == 1){
+							//TODO motorX.rotateTo(listTwo.get(currentState - 1));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						else{
+							//currentState is greater than 1
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 	 
+						}	
+					}
+					Delay.msDelay(2000);
+				}
+				
+				else if (in.equals("wingMirrorLeftX")){
+					System.out.println("Waiting for wingMirrorLeftX...");
+					
+					listOne.set(2, Integer.parseInt(input.readLine()));
+					
+					while (true){
+						
+						if (currentState == 5){
+							//TODO motorX.rotateTo(listOne.get(4);
+							break;
+						}
+						
+						else if ((currentState +1) % 2 == 1){
+							//TODO motorX.rotateTo(listOne.get(currentState + 1));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						else{
+							//currentState is smaller than 5
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 	 
+						}	
+					}
+					Delay.msDelay(2000);
+				}
+				
+				else if (in.equals("radioStation")){
+					System.out.println("Waiting for radioStation...");
+					
+					radioStation = input.readLine();
+					
+					//TODO play small mp3 file if possible
+					
+					
+					
+					
+					Delay.msDelay(2000); //TODO set delay to autiofile length
+				}
+				
+				else if (in.equals("steeringWheelDepth")){
+					System.out.println("Waiting for steeringWheelDepth...");
+					
+					listOne.set(1, Integer.parseInt(input.readLine()));
+					
+					while (true){
+						
+						if (currentState == 3){
+							//TODO motorX.rotateTo(listOne.get(2);
+							break;
+						}
+						
+						else if (currentState == 1){
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;
+							//TODO motorX.rotateTo(listOne.get(2));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						
+						else if (currentState == 5){
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;
+							//TODO motorX.rotateTo(listOne.get(2));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction;	
+						}
+					}
+					
+					Delay.msDelay(2000);
+				}
+				
+				else if (in.equals("steeringWheelTilt")){
+					System.out.println("Waiting for steeringWheelTilt...");
+					
+					listOne.set(0, Integer.parseInt(input.readLine()));
+
+					while (true){
+						
+						if (currentState == 1){
+							//TODO motorX.rotateTo(listOne.get(0);
+							break;
+						}
+						
+						else if ((currentState -1) % 2 == 1){
+							//TODO motorX.rotateTo(listOne.get(currentState - 1));
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 
+						}
+						else{
+							//currentState is greater than 1
+							//TODO stateChanger.rotate(angle that is 1 cm on lego) and right direction; 	 
+						}	
+					}
+					Delay.msDelay(2000);
+				}
+				
 				else if (in.equals("quit")){
 					System.out.println("Closing...");
-					ps.print("EV3 is closing... \n");
-					Delay.msDelay(1500);
-					seat.close();
+					stateChanger.close();
+					mediumMotor.close();
+					largeMotorOne.close();
+					largeMotorTwo.close();
 					servSock.close();
 					ongoing = false;
 				}
+				
 				else{
 					System.out.println("Input empty or unknown command.");
 					ps.println("Input empty or unknown command.\n");
@@ -70,87 +347,3 @@ public class DemoCar{
 		
 	}
 }
-
-/*
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
-import lejos.hardware.Button;
-import lejos.hardware.motor.EV3MediumRegulatedMotor;
-import lejos.hardware.port.MotorPort;
-import lejos.robotics.RegulatedMotor;
-import lejos.utility.Delay;
-
-public class DemoCar {
-
-	private static int angle = 50;
-	private static boolean pressed = false;
-	
-	public static void main(String[] args) {
-		
-		RegulatedMotor seat = new EV3MediumRegulatedMotor(MotorPort.A);
-
-		try{
-			Socket socket = new Socket("127.0.0.1", 4321); //computername
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-			Delay.msDelay(3000);
-			
-			String fromServer = null;
-			System.out.println("1");
-			while ((fromServer = in.readLine()) != null) {
-			    System.out.println("Server: " + fromServer);
-			    if (fromServer.equals("Bye."))
-			        break;
-			}
-			
-			System.out.println("2");
-			
-			while (!pressed){
-				String input = in.readLine();
-				System.out.println("here");
-				System.out.println(input);
-				
-				if (input.equals("") || input.equals(null)){
-					continue;
-				}
-				else if (input.equals("seat")){
-					seat.rotate(angle);
-					Delay.msDelay(2000);
-					angle = angle * -1;
-				}
-				else if (input.equals("quit")){
-					pressed = true;
-					seat.close();
-				}
-				else if (Button.ENTER.isDown()){
-					pressed = true;
-					seat.close();
-					System.exit(1);
-				}
-			}
-		}
-		catch (UnknownHostException e) {
-		   System.out.println("Unknown host: Laptop");
-		   Delay.msDelay(3000);
-		   System.exit(1);
-		}
-		catch  (IOException e) {
-			System.out.println("No I/O");
-			Delay.msDelay(3000);
-			System.exit(1);
-		}
-		
-		System.out.println("Bye Bye!");
-		Delay.msDelay(3000);
-		System.exit(1);
-		
-	}
-	
-
-	
-}
-*/
